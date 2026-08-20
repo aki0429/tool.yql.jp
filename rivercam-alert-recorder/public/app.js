3d6adc499656fca37e0d811c457a981c02e492ee
アッキー.net <96721364+aki0429@users.noreply.github.com>
2026-08-21 08:08:47 +0900

    都道府県を北から南の順に変更

--- rivercam-alert-recorder/public/app.js
+++ rivercam-alert-recorder/public/app.js
@@ -4,7 +4,9 @@ const $ = id => document.getElementById(id);
 
 async function init() {
   cameras = await fetch('/api/cameras').then(response => response.json());
-  const prefectures = [...new Set(cameras.map(camera => camera.prefecture).filter(Boolean))].sort((a, b) => a.localeCompare(b, 'ja'));
+  const northToSouth = ['北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県','茨城県','栃木県','群馬県','埼玉県','千葉県','東京都','神奈川県','新潟県','富山県','石川県','福井県','山梨県','長野県','岐阜県','静岡県','愛知県','三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県','鳥取県','島根県','岡山県','広島県','山口県','徳島県','香川県','愛媛県','高知県','福岡県','佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県'];
+  const available = new Set(cameras.map(camera => camera.prefecture).filter(Boolean));
+  const prefectures = northToSouth.filter(name => available.has(name));
   for (const name of prefectures) $('prefecture').add(new Option(name, name));
   setDefaultTimes();
   bind();
